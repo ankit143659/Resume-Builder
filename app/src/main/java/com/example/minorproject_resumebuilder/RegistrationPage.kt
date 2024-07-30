eb8544a2259f17fa50f035d3eaf3bd93cd204c3e
Ankit Singh <ankitsingh14175@gmail.com>
2024-07-30 12:59:15 +0530

    Resume Builder

--- app/src/main/java/com/example/minorproject_resumebuilder/RegistrationPage.kt
+++ app/src/main/java/com/example/minorproject_resumebuilder/RegistrationPage.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class RegistrationPage : AppCompatActivity() {
@@ -12,10 +13,25 @@ class RegistrationPage : AppCompatActivity() {
         setContentView(R.layout.activity_registration_page)
         val login: Button =findViewById(R.id.buttonRegister);
         val regis: TextView =findViewById(R.id.textViewLogin);
+        val username : EditText = findViewById(R.id.editTextUsername)
+        val emailId : EditText = findViewById(R.id.editTextEmail)
+        val password : EditText = findViewById(R.id.editTextPassword)
+        val cpassword : EditText = findViewById(R.id.editTextConfirmPassword)
 
         login.setOnClickListener{
-            val I= Intent(this,LoginPage::class.java)
-            startActivity(I)
+            if(username.length()==0 || emailId.length()==0 || password.length()==0){
+                username.setError("Username required")
+                emailId.setError("EmailId required")
+                password.setError("Password required")
+
+            }
+            else if(cpassword!==password){
+                cpassword.setError("Password does not match")
+            }
+            else{
+                val I= Intent(this,LoginPage::class.java)
+                startActivity(I)
+            }
         }
         regis.setOnClickListener{
             val I= Intent(this,LoginPage::class.java)
