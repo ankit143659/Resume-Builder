1c1c288545abf113528bc4b1234ed6bab431a26a
Ankit <ankitsingh14175@gmail.com>
2024-08-10 16:37:56 +0530

    comitttttttttttt

--- app/src/main/java/com/example/minorproject_resumebuilder/Create.kt
+++ app/src/main/java/com/example/minorproject_resumebuilder/Create.kt
@@ -1,10 +1,14 @@
 package com.example.minorproject_resumebuilder
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,10 +23,21 @@ private const val ARG_PARAM2 = "param2"
 class Create : Fragment() {
 
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_create, container, false)
+
+        val view :View= inflater.inflate(R.layout.fragment_create, container, false)
+
+        val Btn : LinearLayout = view.findViewById(R.id.personal)
+        Btn.setOnClickListener {
+            val intent = Intent(activity, Basic_personal_details::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }
+
+        return view
     }
 }
