af258481470c9cbc0825f353c0487a4c140d2a70
Ankit Singh <ankitsingh14175@gmail.com>
2024-08-16 21:12:59 +0530

    comittttttt

--- app/src/main/java/com/example/minorproject_resumebuilder/SharePrefrence.kt
+++ app/src/main/java/com/example/minorproject_resumebuilder/SharePrefrence.kt
@@ -2,10 +2,13 @@ package com.example.minorproject_resumebuilder.com.example.minorproject_resumebu
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.os.Build
+import androidx.annotation.RequiresApi
 
 class SharePrefrence (context: Context) {
     private val prefs : SharedPreferences = context.getSharedPreferences("user_prefs",Context.MODE_PRIVATE)
 
+    @RequiresApi(Build.VERSION_CODES.GINGERBREAD)
     fun setLoggedIn(isLoggedin :Boolean){
         val editor = prefs.edit()
         editor.putBoolean("isLoggedIn",isLoggedin)
@@ -16,11 +19,13 @@ class SharePrefrence (context: Context) {
         return prefs.getBoolean("isLoggedIn",false)
     }
 
+    @RequiresApi(Build.VERSION_CODES.GINGERBREAD)
     fun saveUserDetails(userDetails : Map<String,String>){
         val editor = prefs.edit()
         editor.putString("username",userDetails["username"])
         editor.putString("email",userDetails["email"])
         editor.putString("phone",userDetails["phone"])
+        editor.apply()
     }
 
     fun getUsername() : String{
