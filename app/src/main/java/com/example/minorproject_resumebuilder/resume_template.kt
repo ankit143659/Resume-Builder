6baef94e6c9befd956ac48c1dd306b091573b572
Ankit Singh <ankitsingh14175@gmail.com>
2024-08-19 00:45:18 +0530

    comittttttt

--- app/src/main/java/com/example/minorproject_resumebuilder/resume_template.kt
+++ app/src/main/java/com/example/minorproject_resumebuilder/resume_template.kt
@@ -1,6 +1,9 @@
 package com.example.minorproject_resumebuilder
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,13 @@ class resume_template : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val save : Button = findViewById(R.id.save)
+        save.setOnClickListener{
+            Toast.makeText(this,"Successfully filled Data", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this,Create_resume::class.java)
+            startActivity(intent)
         }
     }
 }
