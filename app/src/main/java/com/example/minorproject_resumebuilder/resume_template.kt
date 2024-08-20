137e8753fbb7afa4fa606eec58000d8b2c03067e
Ankit <ankitsingh14175@gmail.com>
2024-08-20 15:18:15 +0530

    comittttt

--- app/src/main/java/com/example/minorproject_resumebuilder/resume_template.kt
+++ app/src/main/java/com/example/minorproject_resumebuilder/resume_template.kt
@@ -1,8 +1,14 @@
 package com.example.minorproject_resumebuilder
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +16,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class resume_template : AppCompatActivity() {
+
+    private lateinit var spinnerItems: Spinner
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,6 +27,52 @@ class resume_template : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        spinnerItems = findViewById(R.id.spinner_items)
+        val img1 : ImageView = findViewById(R.id.img1)
+        val img2 : ImageView = findViewById(R.id.img2)
+        val img3 : ImageView = findViewById(R.id.img3)
+        val img4 : ImageView = findViewById(R.id.img4)
+
+        val items = arrayOf("Select an item", "MEDICAL", "ENGINEERING", "IT FIELD","UNDER GRADUATE","GRADUATE")
+
+
+        val adapter = ArrayAdapter(this,R.layout.simple_spinner_items)
+
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerItems.adapter = adapter
+
+        spinnerItems.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedItem = parent.getItemAtPosition(position) as String
+                if (position == 1) {
+                    img1.visibility=View.VISIBLE
+                    img2.visibility=View.VISIBLE
+                    img4.visibility=View.GONE
+                    img3.visibility=View.GONE
+                } else if (position == 2) {
+                    img1.visibility=View.GONE
+                    img2.visibility=View.GONE
+                    img4.visibility=View.VISIBLE
+                    img3.visibility=View.VISIBLE
+                }
+                else{
+                    img1.visibility=View.GONE
+                    img2.visibility=View.GONE
+                    img4.visibility=View.GONE
+                    img3.visibility=View.GONE
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+
+                img1.visibility=View.GONE
+                img2.visibility=View.GONE
+                img4.visibility=View.GONE
+                img3.visibility=View.GONE
+            }
         }
 
         val save : Button = findViewById(R.id.save)
